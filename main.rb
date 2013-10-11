2837f313868229c3f79011b560356c4960128ef7
Matt Clement <darkmoves@gmail.com>
2013-10-11 17:22:20 -0400

    3 of 4 methods added.

--- main.rb
+++ main.rb
@@ -22,11 +22,36 @@ end
 # 	t.timestamp :created_at
 # end
 
+# Gets all links and displays them on the index page. 
 get '/shortlinks' do
 	@links = Link.all
 	erb :link_index
 end
 
-get '/shortlinks.new' do
-	erb :link_new
+# Gets one link's info and goes to the view/discuss page. 
+get '/shortlinks/:id' do
+	@link = Link.find(params[:id])
+	erb :link_solo
 end
+
+#link_solo.erb cancel comment and redirect to link_index.erb. just use the '/shortlinks' method.
+# get
+# end
+
+#link_index.erb shorten link via submit button, redirects to link_index.erb
+post '/shortlinks/shorten' do
+	#???
+	redirect '/shortlinks'
+end
+
+#link_solo.erb submit comment and redirect to link_index.erb
+post 'shortlinks/comment'
+	Comment.create(params[:id], author: params[:author], body: params[:body])
+	redirect '/shortlinks'
+end
+
+
+
+
+
+
