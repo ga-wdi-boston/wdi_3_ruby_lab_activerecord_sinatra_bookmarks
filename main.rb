85eb3acbde952e17ac0667cb083cc928196d0115
Abby Howell <abby.g.howell@gmail.com>
2013-10-11 15:02:11 -0400

    boilerplate

---
+++ main.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'pg'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+
+set :database, {adapter: 'postgresql',
+				database: 'url-shortener'
+				host: 'localhost'}
+
