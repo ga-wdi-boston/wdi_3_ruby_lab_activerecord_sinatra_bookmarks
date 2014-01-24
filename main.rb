e99b7dc7a2fb2ac3c2817ad32a10cd2be6b454bf
Mike Stone <mikestone14@gmail.com>
2014-01-24 17:23:40 -0500

    Added contacts to the bookmarks hw

--- main.rb
+++ main.rb
@@ -3,20 +3,33 @@ require 'sinatra/reloader'
 require 'pg'
 
 # This helps us run our sql commands
-def run_sql(sql, dbname)
-	db = PG.connect(dbname: dbname, host: 'localhost')
+def run_sql(sql)
+	db = PG.connect(dbname: 'bookmarks', host: 'localhost')
 	result = db.exec(sql)
 	db.close
 	result
 end
 
 get '/bookmarks' do
-	@bookmarks = run_sql("SELECT * FROM bookmarks ORDER BY id DESC", 'bookmarks')
+	@title = 'bookarks'
+	@bookmarks = run_sql("SELECT * FROM bookmarks ORDER BY id DESC")
 	erb :bookmarks
 end
 
 post '/bookmarks' do
 	category, url = params[:category], params[:url]
-	run_sql("INSERT INTO bookmarks (category, url) VALUES ('#{category}', '#{url}')", 'bookmarks')
+	run_sql("INSERT INTO bookmarks (category, url) VALUES ('#{category}', '#{url}')")
 	redirect to '/bookmarks'
+end
+
+get '/contacts' do
+	@title = 'contacts'
+	@contacts = run_sql("SELECT * FROM contact ORDER BY name")
+	erb :contacts
+end
+
+post '/contacts' do
+	name, phone, email = params[:name], params[:phone], params[:email]
+	run_sql("INSERT INTO contact (name, phone, email) VALUES ('#{name}', '#{phone}', '#{email}')")
+	redirect to '/contacts'
 end
