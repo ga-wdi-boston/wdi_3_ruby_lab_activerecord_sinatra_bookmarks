75dd9ec0648a33a297dc29184ab9d79ac4f91330
emreiser <emilie.reiser@gmail.com>
2014-01-26 13:14:12 -0500

    Seeded db

--- setup.sql
+++ setup.sql
@@ -1,3 +1,7 @@
+-- ******
+-- Name of database is 'bookmarks'
+-- ******
+
 DROP TABLE IF EXISTS bookmarks;
 
 CREATE TABLE bookmarks (
@@ -8,3 +12,6 @@ CREATE TABLE bookmarks (
 	);
 
 INSERT INTO bookmarks (name, url, tag) VALUES ('Google', 'http://google.com', 'Search');
+INSERT INTO bookmarks (name, url, tag) VALUES ('CNN', 'http://cnn.com', 'News');
+INSERT INTO bookmarks (name, url, tag) VALUES ('NY Times', 'http://www.nytimes.com/', 'News');
+INSERT INTO bookmarks (name, url, tag) VALUES ('DNS', 'http://www.petekeen.net/dns-the-good-parts', 'Article');
